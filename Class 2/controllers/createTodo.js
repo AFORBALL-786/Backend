c29f992bff7e16d84f3b36ec55a682057c936244
Ansari Irshad <133130589+AFORBALL-786@users.noreply.github.com>
2023-10-23 17:26:29 +0545

    Add files via upload

--- Class 2/controllers/createTodo.js
+++ Class 2/controllers/createTodo.js
@@ -1,26 +1,21 @@
-// import the schema
-const company = require('../model/todoSchema');
+const todo = require('../model/todoSchema')
 
 exports.createTodo = async(req,res) => {
     try{
-        // fetch the data from request body
-        const {firstname,lastname,age,phone,description} = req.body;
-
-        // insert in the database
-        await company.create({firstname,lastname,age,phone,description});
-
-        // sending response
+        const {name,age,description} = req.body;
+        await todo.create({name,age,description});
+        
         res.status(200).json({
             sucess:true,
-            message:"Entry Created Sucessfully"
+            message:"Data Entry Sucessfully"
         })
 
     } catch(error){
-        console.log(error);
-        console.error(error);
+        console.log(error)
         res.status(500).json({
             sucess:false,
-            message : "Internal Server Error",
+            message:"Internal Server",
         })
     }
+
 }
