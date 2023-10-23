6f54199e9c41db82e219b07bbdc455fa2b2d67c3
Ansari Irshad <133130589+AFORBALL-786@users.noreply.github.com>
2023-10-23 17:22:55 +0545

    Add files via upload

--- Class 2/model/todoSchema.js
+++ Class 2/model/todoSchema.js
@@ -2,29 +2,17 @@ const mongoose = require('mongoose');
 
 const todoSchema = new mongoose.Schema(
     {
-        firstname:{
+        name:{
             type:String,
             require:true,
-            maxlength:15,
-        },
-        lastname:{
-            type:String,
-            require:true,
-            maxlength:15,
         },
         age:{
             type:Number,
             require:true,
         },
-        phone:{
-            type:Number,
-            require:true,
-            minlength:10, 
-        },
         description:{
             type:String,
             require:true,
-            maxlength:30
         },
         createAt:{
             type:Date,
@@ -36,8 +24,8 @@ const todoSchema = new mongoose.Schema(
             require:true,
             default:Date.now()
         },
-        
     }
 )
 
-module.exports = mongoose.model("Schema", todoSchema);
+
+module.exports = mongoose.model('TodoData', todoSchema);
