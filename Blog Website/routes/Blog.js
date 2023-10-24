e24009b9ed6f014fe72a0af85e3296cd7ccf2c64
Ansari Irshad <133130589+AFORBALL-786@users.noreply.github.com>
2023-10-24 16:27:17 +0545

    updated route

--- Blog Website/routes/Blog.js
+++ Blog Website/routes/Blog.js
@@ -3,9 +3,19 @@ const router = express.Router();
 
 // import controller
 const {createComment} = require("../controller/CommentController");
-
+const {likePost} =require("../controller/LikeController");
+const {unlikePost} = require("../controller/UnlikeController");
+const {deleteComment} = require("../controller/DeleteCommentController");
+const {createPost} = require("../controller/CreatePost");
+const {getPost} = require("../controller/GetPost");
+ 
 // mapping
 router.post('/comments/create', createComment);
+router.post('/likes/like', likePost);
+router.post('/likes/unlike', unlikePost);
+router.post('/comments/deletecomment', deleteComment);
+router.post('/posts/create', createPost);
+router.get('/posts', getPost);
 
 // export router
 module.exports = router;
